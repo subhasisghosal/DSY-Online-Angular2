9013f087d55a0b0844a0372d8ae37007256ca911
subhasisghosal <supernovasubho@yahoo.in>
2019-04-08 01:37:47 +0530

    user list and detail endpoints working

--- routes/main-routes.js
+++ routes/main-routes.js
@@ -4,6 +4,5 @@ function init(app) {
     const path = '/operation'
     app.get(path + '/admission', operation.getAdmissionForm)
     app.post(path + '/admission', operation.saveAdmissionForm)
-        // app.get(path + '/', rating.checkHealth)
 }
 module.exports = init;
