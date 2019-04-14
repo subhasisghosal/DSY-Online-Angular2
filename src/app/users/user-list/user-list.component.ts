67c713a679a5c99ef528eb83484b9aca66976da6
subhasisghosal <supernovasubho@yahoo.in>
2019-04-15 00:48:46 +0530

    creating CRUD operations on User model

--- src/app/users/user-list/user-list.component.ts
+++ src/app/users/user-list/user-list.component.ts
@@ -1,15 +1,74 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../user';
+import { UserService } from '../user.service';
+import { UserDetailsComponent } from '../user-details/user-details.component';
 
 @Component({
   selector: 'app-user-list',
   templateUrl: './user-list.component.html',
-  styleUrls: ['./user-list.component.css']
+  styleUrls: ['./user-list.component.css'],
+  providers: [UserService]
 })
+
 export class UserListComponent implements OnInit {
 
-  constructor() { }
+  users: User[]
+  selectedUser: User
+
+  constructor(private userService: UserService) { }
 
   ngOnInit() {
+    this.userService
+      .getUsers()
+      .then((users: User[]) => {
+        this.users = users.map((user) => {
+          if (!user.user_type) {
+            user.user_type = 'All';
+          }
+          return user;
+        });
+      });
   }
 
+  selectUser(user: User) {
+    this.selectedUser = user;
+  }
+
+  createNewUser() {
+    let user: User = {
+      name: '',
+      email: '',
+      phone: {
+        work: '',
+        mobile: ''
+      }
+    };
+
+    // By default, a newly-created contact will have the selected state.
+    this.selectUser(user);
+  }
+
+  deleteUser = (userId: String) => {
+    var idx = this.getIndexOfUser(userId);
+    if (idx !== -1) {
+      this.users.splice(idx, 1);
+      this.selectUser(null);
+    }
+    return this.users;
+  }
+
+  addUser = (user: User) => {
+    this.users.push(user);
+    this.selectUser(user);
+    return this.users;
+  }
+
+  updateUser = (user: User) => {
+    var idx = this.getIndexOfUser(user._id);
+    if (idx !== -1) {
+      this.users[idx] = user;
+      this.selectUser(user);
+    }
+    return this.users;
+  }
 }
--- src/app/users/user.service.ts
+++ src/app/users/user.service.ts
@@ -1,8 +1,52 @@
 import { Injectable } from '@angular/core';
+import { User } from './user';
+import { Http, Response } from '@angular/http';
 
 @Injectable()
 export class UserService {
+  private usersUrl = '/admin/user/list';
+  private admissionUrl = '/operation/admission';
+  private userUrl = '/admin/user';
 
-  constructor() { }
+  constructor(private http: Http) { }
 
+  // get("/api/contacts")
+  getUsers(): Promise<void | User[]> {
+    return this.http.get(this.usersUrl)
+      .toPromise()
+      .then(response => response.json() as User[])
+      .catch(this.handleError);
+  }
+
+  // post("/api/contacts")
+  createUser(newUser: User): Promise<void | User> {
+    return this.http.post(this.admissionUrl, newUser)
+      .toPromise()
+      .then(response => response.json() as User)
+      .catch(this.handleError);
+  }
+
+  // get("/api/contacts/:id") endpoint not used by Angular app
+
+  // delete("/api/contacts/:id")
+  deleteUser(delUserId: String): Promise<void | String> {
+    return this.http.delete(this.userUrl + '/' + delUserId)
+      .toPromise()
+      .then(response => response.json() as String)
+      .catch(this.handleError);
+  }
+
+  // put("/api/contacts/:id")
+  updateUser(putUser: User): Promise<void | User> {
+    return this.http.put(this.userUrl + '/' + putUser.id, putUser)
+      .toPromise()
+      .then(response => response.json() as User)
+      .catch(this.handleError);
+  }
+
+  private handleError(error: any) {
+    const errMsg = (error.message) ? error.message :
+      error.status ? `${error.status} - ${error.statusText}` : 'Server error';
+    console.error(errMsg); // log to console instead
+  }
 }
